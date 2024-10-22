1b78a103ce54dcc65c50d6a223ecef4ff49d3d1b
gabe-ki70 <gabriel_koerner-imas26@milton.edu>
2024-10-22 15:22:15 -0400

    Printed more data about pokemon

--- src/ReadJson.java
+++ src/ReadJson.java
@@ -5,6 +5,18 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.SQLOutput;
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Scanner;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JScrollPane;
 
 
 // video to load jar
@@ -16,9 +28,11 @@ import org.json.simple.parser.ParseException;
 
 // Program for print data in JSON format.
 public class ReadJson {
-    public static void main(String args[]) throws ParseException {
+    public static void main(String args[]) throws ParseException{
         // In java JSONObject is used to create JSON object
         // which is a subclass of java.util.HashMap.
+       // JFrame mainFrame;
+
 
         JSONObject file = new JSONObject();
         file.put("Full Name", "Ritu Sharma");
@@ -38,7 +52,7 @@ public class ReadJson {
         String totlaJson="";
         try {
 
-            URL url = new URL("https://pokeapi.co/api/v2/pokemon/ditto");
+            URL url = new URL("https://pokeapi.co/api/v2/pokemon/bulbasaur");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
@@ -74,22 +88,34 @@ public class ReadJson {
         System.out.println(jsonObject);
 
         try {
-
+            String name = (String)jsonObject.get("name");
+            System.out.println("name: "+name);
             //String name = (String)jsonObject.get("name");
+            long weight = (long) jsonObject.get("weight");
+            System.out.println("weight: "+weight);
 
             org.json.simple.JSONArray msg = (org.json.simple.JSONArray) jsonObject.get("abilities");
             int n =   msg.size(); //(msg).length();
             for (int i = 0; i < n; ++i) {
                 JSONObject test = (JSONObject) msg.get(i);
-                System.out.println(test);
+                //System.out.println(test);
                 JSONObject abilities = (JSONObject) test.get("ability");
                 String abilities1 = (String)abilities.get("name");
                 System.out.println("ability: "+abilities1);
                 // System.out.println(person.getInt("key"));
             }
 
-            String name = (String)jsonObject.get("name");
-            System.out.println("name: "+name);
+            org.json.simple.JSONArray msg1 = (org.json.simple.JSONArray) jsonObject.get("moves");
+            for (int s = 0; s < n; ++s){
+                JSONObject test1 = (JSONObject) msg1.get(s);
+                //System.out.println(test);
+                JSONObject moves = (JSONObject) test1.get("move");
+                String moves1 = (String)moves.get("name");
+                System.out.println("move: "+moves1);
+                // System.out.println(person.getInt("key"));
+            }
+
+
            // org.json.simple.JSONArray msg = (org.json.simple.JSONArray) jsonObject.get("abilities");
 
 
